75cfa2fb91c37a75805974605148f7880c15fd1c
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-05-12 19:45:17 +0400

    com

---
+++ FromWebFormToJson/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using FromWebFormToJson.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text.Json;
+
+namespace FromWebFormToJson.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult AddPerson()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPerson(Person person)
+        {
+
+            if (person.MiddleName == null || person.FirstName == null || person.LastName == null || person.BirthDate == null)
+            {
+                TempData["msg"] = "Fill in all the fields";
+                return View();
+            }
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(person, options);
+            System.IO.File.WriteAllText("person.json", json);
+
+            TempData["msg"] = "Added successfully";
+
+            return View();
+        }
+    }
+}
---
+++ FromWebFormToJson/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+
+namespace FromWebFormToJson.Models
+{
+    public class Person
+    {
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? MiddleName { get; set; }
+        public DateOnly? BirthDate { get; set; }
+    }
+}
